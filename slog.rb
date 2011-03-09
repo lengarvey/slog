fa3c445c2b7e7787044444d95081448f40347bbd
Leonard Garvey <lengarvey@gmail.com>
2011-03-09 23:05:00 +1100

    Initial Commit
    Site very functional.
    
    1. Login/Authentication
    2. Staging of Articles
    3. Reformatting of Add
    4. Generation of Tag Cloud
    4a. Adding tags to articles
    5. Allowing some form of markup in articles to ease publishing
    6. Disallowing any markup in posts. Links?
    7. Preventing spam?

---
+++ slog.rb
@@ -0,0 +1,70 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+require 'lib/partial/partials.rb'
+require 'mongo_mapper'
+
+MongoMapper.database = 'slog'
+
+class Article
+  include MongoMapper::Document
+
+  key :title, String
+  key :text, String
+  key :date, Time
+
+  key :author, String
+
+  key :tags, Array
+
+  many :comments
+
+end
+
+class Comment
+  include MongoMapper::EmbeddedDocument
+
+  key :date, Time
+  key :author, String
+  key :text, String
+
+end
+
+helpers Sinatra::Partials
+
+get '/article/add' do
+  haml :add_article
+end
+post '/article/add' do
+  @article = Article.new
+  @article.title = params[:title]
+  @article.text = params[:text]
+  @article.date = Time.now
+  @article.save
+
+  haml :article
+end
+get '/article/:id' do |id|
+  @article = Article.where(:id => id).first()
+  haml :view_article
+end
+post '/article/:id/comment' do |id|
+  @article = Article.where(:id => id).first()
+  pass unless @article
+  comment = Comment.new
+  comment.author = params[:author]
+  comment.date = Time.now
+  comment.text = params[:comment]
+  @article.comments << comment
+  @article.save
+  haml :view_article
+
+end
+get '/' do
+  @articles = Article.sort(:date.desc).all()
+  haml :index
+end
+
+get '/*' do
+  "Blank page"
+end
